5d5888106bcb5602f038e29cefc99023b0af80d8
luk2009 <lucianomenendez07@gmail.com>
2020-05-26 22:33:36 -0400

    primer commit archivo python

---
+++ renombrar.py
@@ -0,0 +1,18 @@
+import os
+import re
+carpeta_ficheros=input("ruta ")
+for original in os.listdir(carpeta_ficheros):
+#("c:\\youtubedl\\PHP"):
+	#carpeta_ficheros="c:\\youtubedl\\PHP\\"
+	numeritos=re.findall(r"[-+]?\d*\.\d+|\d+", original)
+	#var_num=numeritos[0]
+	#print(original)
+	#print(numeritos[0])
+	cambio=str(numeritos[0])+"-"+original
+	print(carpeta_ficheros+cambio)
+	print(str(numeritos[0])+"-"+original)
+	os.rename(carpeta_ficheros+original, carpeta_ficheros+cambio)
+
+#os.rename("C:\\youtubedl\\PHP\\Curso PHP MySQL. Ámbitos de las variables. Vídeo 6-wcNXqTkISvg.mp4","C:\\youtubedl\\PHP\\6-Curso PHP MySQL. Ámbitos de las variables. Vídeo 6-wcNXqTkISvg.mp4")	
+
+
